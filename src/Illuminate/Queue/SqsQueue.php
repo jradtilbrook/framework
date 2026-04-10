dfdc4008072d9363a3ef90a258065134cba4eb37
jradtilbrook <jarryd@tilbrooktech.com>
2026-04-10 10:53:55 +0800

    Comment

--- src/Illuminate/Queue/SqsQueue.php
+++ src/Illuminate/Queue/SqsQueue.php
@@ -341,9 +341,12 @@ class SqsQueue extends Queue implements QueueContract, ClearableQueue
         if (! is_null($response['Messages']) && count($response['Messages']) > 0) {
             if (laravel_cloud()) {
                 $wait = $response['Messages'][0]['Attributes']['ApproximateFirstReceiveTimestamp'] - $response['Messages'][0]['Attributes']['SentTimestamp'];
+
                 if ($response['Messages'][0]['Attributes']['ApproximateReceiveCount'] > 1) {
                     $wait = null;
                 }
+
+                // SQS timestamps are in milliseconds
                 CloudQueueEventEmitter::processing($wait / 1000);
             }
 
